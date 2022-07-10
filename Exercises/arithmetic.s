9eab16c4cbfa8e66e7f6fa00d39631320bff5f61
Derek Sneddon <dsneddon00@gmail.com>
2022-07-09 21:45:15 -0400

    Finished Arithmetic Exercises

---
+++ Exercises/arithmetic.s
@@ -0,0 +1,9 @@
+global _start
+
+section .text
+_start:
+	xor rax, 	rax
+	xor rbx, 	rbx
+	add rbx, 	15
+	xor rbx,	15
+
--- Fibonacci/fibonacci.s
+++ Fibonacci/fibonacci.s
@@ -2,6 +2,12 @@ global  _start
 
 section .text
 _start:
-	mov al,		0
-	mov bl,		1
+	;mov al,		0
+	;mov bl,		0
+	;inc bl ; inc unary operator increases bl by 1
+	;add rax, rbx ; add binary operator adds rbx to rax
 	
+	xor rax, rax ; using xor we can set rax to 0 by comparing it to itself
+	xor rbx, rbx
+	inc rbx
+	add rax, rbx
