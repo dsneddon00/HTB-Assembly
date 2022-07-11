7233a2b4fd7d56defb58c257ea1fa8de7ea8a4a7
Derek Sneddon <dsneddon00@gmail.com>
2022-07-11 13:02:40 -0400

    Loops section finished

--- Fibonacci/fibonacci.s
+++ Fibonacci/fibonacci.s
@@ -2,12 +2,15 @@ global  _start
 
 section .text
 _start:
-	;mov al,		0
-	;mov bl,		0
-	;inc bl ; inc unary operator increases bl by 1
-	;add rax, rbx ; add binary operator adds rbx to rax
-	
+	; inital value of values of fibonacchi sequence (0, 1)
 	xor rax, rax ; using xor we can set rax to 0 by comparing it to itself
-	xor rbx, rbx
-	inc rbx
-	add rax, rbx
+	xor rbx, rbx ; initializing both rax and rbx to 0
+	inc rbx ; increase rbx by one
+	mov rcx, 10 ; iterate the loop 10 times
+
+fibLoop:
+	add rax, rbx ; get next next number of fib sequence (E.G. 0 + 1 = 1, 1 + 1 = 2, 1 + 2 = 3, ect.)
+	; also consider that the result is stored in rax, so 0, 1 will become 1, 1)
+	xchg rax, rbx ; swap the two values to put the higher value in rbx
+	loop fibLoop
+
