6c68feaac34e4834a92eeb8ef20ba6948f2bd727
Derek Sneddon <dsneddon00@gmail.com>
2022-07-12 20:00:32 -0400

    finished function exercises

--- Fibonacci/fibonacci.s
+++ Fibonacci/fibonacci.s
@@ -1,17 +1,19 @@
 global  _start
+extern printf
 
 section .data
 	message db "Fibonacci Sequence:", 0x0a ; 0x0a is the new line character
+	output db "%d", 0x0a, 0x00 ; %d prints out an integer, 0x0a is \n, and 0x00 is a null character for terminating printf
 
 
 section .text
 _start:
-	call print
+	call printLabel
 	call init
 	call fibLoop
 	call exit
 
-print:
+printLabel:
 	mov rax, 1 ; rax: syscall 1
 	mov rdi, 1 ; using 1 for stdout
 	mov rsi, message ; pointing to message from .data
@@ -26,7 +28,18 @@ init:
 	inc rbx ; increase rbx by one
 	ret	
 
+printFib:
+	push rax ; push register to the stack
+	push rbx ; push register to the stack
+	mov rdi, output ; 1st argument (printing format)
+	mov rsi, rbx ; 2nd argument (Fib num)
+	call printf	
+	pop rbx ; restore register from the stack
+	pop rax ; restore register from the stack
+	ret	
+
 fibLoop:
+	call printFib
 	add rax, rbx ; get next next number of fib sequence (E.G. 0 + 1 = 1, 1 + 1 = 2, 1 + 2 = 3, ect.)
 	; also consider that the result is stored in rax, so 0, 1 will become 1, 1)
 	xchg rax, rbx ; swap the two values to put the higher value in rbx
--- README.md
+++ README.md
@@ -4,15 +4,21 @@ This repo contains my class work and projects with the Assembly Module of Hack t
 
 ### Manually
 
+Without dynamic linking to the libc library.
 ```
 nasm -f elf64 example.s
 ld -o example example.o
 ./example
 ```
+With dynamic linking to the libc library
+```
+nasm -f elf64 example.s &&  ld example.o -o example -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2 && ./example
+```
 
 ### Automated
 ```
-./assembler.sh
+./assembler.sh example.s
+./assembler.sh example.s -g
 ```
 
 ## Disassembly
